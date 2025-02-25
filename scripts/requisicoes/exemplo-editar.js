c6ecd5bf76d6d0004a3ba5ad0baa02430da3562f
Tiago Bazzi Zancanella <tiagozancanella17@gmail.com>
2025-02-25 16:31:40 -0300

    first commit

---
+++ scripts/requisicoes/exemplo-editar.js
@@ -0,0 +1,80 @@
+const campoNome = document.getElementById('campoNome');
+const campoCnpj = document.getElementById('campoCNPJ')
+const mascara = {
+    mask: "00.000.000/0000-00"
+};
+const mask = IMask(campoCnpj, mascara);
+
+
+// https => protocolo
+// dominio => franciscosensaulas.com 
+// porta => 2000
+// recurso => /api/v1/empresa
+// query_params (variáveis) => id (valor do id é 20)
+// http://franciscosensaulas.com:2000/api/v1/empresa?id=20
+
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+const idParaEditar = params.get("id");
+const urlAPI = "https://public.franciscosensaulas.com"
+
+async function consultarDadosEmpresaPorId() {
+    const urlParaConsultarEmpresa = `${urlAPI}/api/v1/empresa/${idParaEditar}`
+    console.log(urlParaConsultarEmpresa);
+
+    const resposta = await fetch(urlParaConsultarEmpresa);
+    if (resposta.ok == false) {
+        alert("Empresa não encontrada");
+        window.location.href = "/exemplo-requisicoes.html";
+        return
+    }
+
+    const dadosEmpresa = await resposta.json();
+    console.log(dadosEmpresa);
+
+    campoNome.value = dadosEmpresa.nome;
+    campoCnpj.value = dadosEmpresa.cnpj;
+
+
+
+
+}
+
+
+async function editar(evento) {
+    evento.preventDefault();
+
+
+    let cnpj = campoCnpj.value;
+    let nome = campoNome.value;
+
+    const dados = {
+        nome: nome,
+        cnpj: cnpj
+    }
+
+    let url = `${urlAPI}/api/v1/empresa/${idParaEditar}`;
+    const resposta = await fetch(url, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(dados)
+    });
+
+    if (resposta.ok == false) {
+        alert("não foi possivel alterar")
+    } else {
+        location.href = '/exemplo-requisicoes.html';
+    }
+
+}
+
+
+
+
+
+
+
+const botaoEditar = document.getElementById("botaoAlterar");
+botaoEditar.addEventListener("click", editar);
+
+consultarDadosEmpresaPorId();
